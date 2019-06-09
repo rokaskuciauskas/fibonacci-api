0879891c374ea8336b8c6eb9d3d8f52f7e35f516
rokaskuciauskas <rokas.kuciauskas@gmail.com>
2019-06-09 18:33:14 +0300

    Add solution

---
+++ Fibonacci/Controllers/FibonacciController.cs
@@ -0,0 +1,25 @@
+﻿using Fibonacci.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Fibonacci.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FibonacciController : ControllerBase
+	{
+		// GET api/fibonacci/10
+		[HttpGet("{length}")]
+		public ActionResult<List<BigInteger>> Get(int length)
+		{
+			if (length > 0 && length <= 100000)
+			{
+				return FibonacciSequenceGenerator.GenerateFibonacciSequence(length);
+			} else
+			{
+				return BadRequest("Parameter should be positive integer between 1 and 100000");
+			}
+		}
+	}
+}
---
+++ Fibonacci/Services/FibonacciSequenceGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Fibonacci.Services
+{
+	public class FibonacciSequenceGenerator
+	{
+		public static List<BigInteger> GenerateFibonacciSequence(int length)
+		{
+			List<BigInteger> sequence = new List<BigInteger>();
+			BigInteger n1 = 0, n2 = 1, lastMember;
+			int i;
+			sequence.Add(n1);
+			sequence.Add(n2);
+			for (i = 2; i < length; ++i)
+			{
+				lastMember = n1 + n2;
+				n1 = n2;
+				n2 = lastMember;
+				sequence.Add(lastMember);
+			}
+			return sequence;
+		}
+	}
+}
